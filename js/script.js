4ae3610565c848786d662108abea7d719f1c1452
Artem <C:\Users\\AppData\Roaming\The Bat!>
2021-12-02 16:20:21 +0300

    Добавил изменение текста под 2 картинкой в "Аксессуарах"

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+if (window.innerWidth >= 320) {
+  document.querySelector(
+    '.property > .accessories > .acccessories-wrapper > .item:nth-child(2) > p.description'
+  ).textContent =
+    'Беспроводная зарядная станция Duo Pad быстро заряжает одновременно два устройства, одно из которых охлаждается.';
+}
+
+if (window.innerWidth >= 720) {
+  document.querySelector(
+    '.property > .accessories > .acccessories-wrapper > .item:nth-child(2) > p.description'
+  ).textContent =
+    'Обширный ассортимент чехлов лля каждой модели. Цветовая гамма, отличное качество, премиальные материалы и дополнительные возможности призваны удовлетворить даже самый притязательный вкус.';
+}
+
 if (window.innerWidth >= 1000) {
   document.querySelector('.property > .shooting-video > h3').textContent =
     'Сканер отпечатка в экране';
@@ -10,11 +24,4 @@ if (window.innerWidth >= 1000) {
       element.textContent =
         'Для любителей роскоши, модель S10+ будет выпускаться в керамическом корпусе черного и белого цветов.';
     });
-}
-
-if (window.innerWidth >= 720) {
-  document.querySelector(
-    '.property > .accessories > .acccessories-wrapper > .item:nth-child(2) > p.description'
-  ).textContent =
-    'Обширный ассортимент чехлов лля каждой модели. Цветовая гамма, отличное качество, премиальные материалы и дополнительные возможности призваны удовлетворить даже самый притязательный вкус.';
 }
